9f079e057cc12c5074d3fa3d02103f05d3e63dfe
volodg@mail.ru <volodg@mail.ru>
2011-07-19 23:34:21 +0300

    commit solution of second task

---
+++ 2/source.hs
@@ -0,0 +1,23 @@
+
+charsCount :: [ ( Char, Int ) ] -> Char -> [ ( Char, Int ) ]
+charsCount [] ch = [ ( ch, 1 ) ]
+charsCount (( ch1, num ):xs) ch2 | ch1==ch2 = ( ch1, num + 1 ):xs
+								 | otherwise = ( ch1, num ):charsCount xs ch2
+
+minimumFrequency :: [ ( Char, Int ) ] -> Int
+minimumFrequency [] = error "undefined result"
+minimumFrequency (( _, num ):[]) = num
+minimumFrequency (( _, num ):xs) | num < minimumFrequency( xs ) = num
+						   		 | otherwise = minimumFrequency( xs )
+
+main :: IO ()
+main = do
+   src <- readFile "source.txt"
+   frequency <- return ( foldl (charsCount) [] src )
+   minimum' <- return ( minimumFrequency frequency )
+   print [ ch | ( ch, num ) <- frequency, num == minimum' ]
+-- ^   print ( charsCount [ ( 'w', 1 ), ( 'b', 1 ), ( 'd', 2 ) ] 's' )
+-- ^   writeFile "file.out" (operate src)
+
+
+-- ^operate = x
