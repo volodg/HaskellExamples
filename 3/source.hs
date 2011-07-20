ffa0ed44e8e63be60126d7aec25807d5a5a8af67
volodg@mail.ru <volodg@mail.ru>
2011-07-20 23:27:28 +0300

    add 4 task link

--- 3/source.hs
+++ 3/source.hs
@@ -1,19 +1,26 @@
-import Char (isUpper)
+import Char (isUpper, isAlpha)
 
 withinGuardsChars :: [ Char ] -> Bool
-withinGuardsChars ( y ) = isUpper( y !! 0 ) && isUpper( y !! 1 )
--- ^    && isUpper( y !! 2 ) && not( isUpper( y !! 3 ) ) && isUpper( y !! 4 ) && isUpper( y !! 5 ) && isUpper( y !! 6 )
+withinGuardsChars ( y ) = not( isUpper( y !! 0 ) )
+ && isUpper( y !! 1 )
+ && isUpper( y !! 2 )
+ && isUpper( y !! 3 )
+ && not( isUpper( y !! 4 ) ) && isAlpha( y !! 4 )
+ && isUpper( y !! 5 )
+ && isUpper( y !! 6 )
+ && isUpper( y !! 7 )
+ && not( isUpper( y !! 8 ) )
 
-withinGuards :: [ Char ] -> [ Char ] -> [ Char ]
-withinGuards ( _ ) [] = []
-withinGuards ( sevenLettersList ) ( char:xs ) | length( sevenLettersList )<7 = withinGuards (char:sevenLettersList) xs
-                                              | withinGuardsChars( sevenLettersList ) = sevenLettersList
-                                              | otherwise = withinGuards ( char:tail( sevenLettersList ) ) xs
+withinGuards :: [ [ Char ] ] -> [ Char ] -> [ Char ] -> [ [ Char ] ]
+withinGuards results ( _ ) [] = [ [ ch | chars <- results, let ch = chars !! 4 ] ]
+withinGuards results ( sevenLettersList ) ( char:xs ) | length( sevenLettersList )<9 = withinGuards results (char:sevenLettersList) xs
+                                                      | withinGuardsChars( sevenLettersList ) = withinGuards ( sevenLettersList:results ) [] xs
+                                                      | otherwise = withinGuards results ( char:reverse( tail( reverse( sevenLettersList ) ) ) ) xs
 
 main :: IO ()
 main = do
    src <- readFile "source.txt"
-   print ( withinGuards [] src )
+   print ( withinGuards [] [] ( reverse( src ) ) )
 -- ^   frequency <- return ( foldl (charsCount) [] src )
 -- ^   minimum' <- return ( minimumFrequency frequency )
 -- ^   print [ ch | ( ch, num ) <- frequency, num == minimum' ]
